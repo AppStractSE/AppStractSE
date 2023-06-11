77d18307d269f5a85e1be80c498d20b96722c094
Erik <erik.grune@gmail.com>
2023-06-11 22:15:29 +0200

    experimental form for netlify

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,7 +2,7 @@ import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import { useLanguage } from "../contexts/LanguageContext";
 
 interface Props {
@@ -27,55 +27,58 @@ const ContactForm = ({ openPolicyInNewTab }: Props) => {
             {translations.contact.title}
           </div>
         </div>
-        <InputText
-          id="name"
-          type="text"
-          placeholder={translations.contact.name}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder"
-        />
-        <div className="flex gap-3">
+        <form name="contact" method="POST" data-netlify="true">
           <InputText
-            id="email"
+            id="name"
             type="text"
-            placeholder={translations.contact.email}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            placeholder={translations.contact.name}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder"
           />
-          <InputText
-            id="phone"
-            type="text"
-            placeholder={translations.contact.phone}
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder"
-          />
-        </div>
-        <InputTextarea
-          value={message}
-          rows={5}
-          placeholder={translations.contact.message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder textarea"
-        />
-        <div className="flex align-items-center justify-content-between mb-4">
-          <div className="text-600 font-medium line-height-2 text-xs md:text-sm">
-            {translations.contact.info}{" "}
-            <Link
-              target={openPolicyInNewTab ? "_blank" : "_self"}
-              className="nav-link p-0 text-primary hover:text-primary-300 cursor-pointer"
-              to={translations.paths.policy.link}
-            >
-              {translations.contact.policy}
-            </Link>
+          <div className="flex gap-3">
+            <InputText
+              id="email"
+              type="text"
+              placeholder={translations.contact.email}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder"
+            />
+            <InputText
+              id="phone"
+              type="text"
+              placeholder={translations.contact.phone}
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder"
+            />
           </div>
-        </div>
-        <Button
-          label={translations.buttons.submit}
-          className="w-full mb-3 shadow-3 hover:shadow-6 py-3"
-        />
+          <InputTextarea
+            value={message}
+            rows={5}
+            placeholder={translations.contact.message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="w-full mb-3 shadow-3 py-3 bg-primary-200 text-black-alpha-90 border-none hover:shadow-6 dark-placeholder textarea"
+          />
+          <div className="flex align-items-center justify-content-between mb-4">
+            <div className="text-600 font-medium line-height-2 text-xs md:text-sm">
+              {translations.contact.info}{" "}
+              <Link
+                target={openPolicyInNewTab ? "_blank" : "_self"}
+                className="nav-link p-0 text-primary hover:text-primary-300 cursor-pointer"
+                to={translations.paths.policy.link}
+              >
+                {translations.contact.policy}
+              </Link>
+            </div>
+          </div>
+          <Button
+            label={translations.buttons.submit}
+            className="w-full mb-3 shadow-3 hover:shadow-6 py-3"
+            type="submit"
+          />
+        </form>
       </div>
     </div>
   );
