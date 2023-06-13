7215532143c0e633237b2a2261122c8189db499b
4l3x <alexander.auermaa@gmail.com>
2023-06-13 18:06:59 +0200

    padding fix

--- src/pages/home/sections/ContactSection.tsx
+++ src/pages/home/sections/ContactSection.tsx
@@ -3,8 +3,8 @@ import ContactForm from "../../../components/ContactForm";
 const ContactSection = () => {
   return (
     <section className="w-full curved-bg  overflow-hidden" id="contact">
-      <div className="md:p-4 lg:p-6 text-800 bg-bluegray-900">
-        <div className="container w-full m-auto pb-6">
+      <div className="p-2 md:p-4 lg:p-6 text-800 bg-bluegray-900">
+        <div className="container w-full m-auto py-6">
           <ContactForm />
         </div>
       </div>
