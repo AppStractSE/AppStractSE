35e86ce54a055f03dfe74ad83ea41b4f05759c12
4l3x <alexander.auermaa@gmail.com>
2023-06-18 15:02:37 +0200

    commented code removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,28 +9,6 @@ const Footer = () => {
             <div className="col-12 md:col-6 flex my-4 md:my-0 justify-content-end gap-2 md:gap-1">
               <Socials />
             </div>
-            {/* <div className="col-12 flex md:col-6 justify-content-between">
-              <div className="col-6 md:col-4 flex justify-content-center md:block">
-                <div className="">
-                  <div className="mb-3 text-primary font-bold">Links</div>
-                  <div>Riktigt lång lääääänk</div>
-                  <div>Länk 2</div>
-                  <div>Länk 3</div>
-                  <div>Länk 4</div>
-                  <div>Länk 5</div>
-                  <div>Länk 6</div>
-                </div>
-              </div>
-              <div className="col-6 md:col-4 flex justify-content-center md:block">
-                <div className="">
-                  <div className="mb-3 text-primary font-bold">Länkar</div>
-                  <div>Länk 1 till ditåt</div>
-                  <div>Länk 2</div>
-                  <div>Länk 3</div>
-                  <div>Länk 4</div>
-                </div>
-              </div>
-            </div> */}
           </div>
           <p className="col-12 md:col-6 text-right md:text-left text-xs mb-0">© 2023 Appstract</p>
         </div>
