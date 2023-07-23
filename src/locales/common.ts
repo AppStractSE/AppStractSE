25fba3e9f49cb52bb67eb49d48064981a4f33061
Erik <erik.grune@gmail.com>
2023-07-23 21:42:22 +0200

    removed placeholders for now

--- src/locales/common.ts
+++ src/locales/common.ts
@@ -39,30 +39,30 @@ const clients: Client[] = [
     slug: 'client-one',
     tags: [clientTags.apps],
   },
-  {
-    title: 'Client2',
-    subtitle: 'A swedish startup that fights food waste.',
-    logo: 'https://justifiedcoding.org/wp-content/uploads/2019/01/Client-Logo-Placeholder.png',
-    image:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
-    description:
-      'Client2 is a Swedish startup that fights food waste by selling surplus food from restaurants, cafes and grocery stores at a lower price.',
-    link: 'https://appstract.se',
-    slug: 'client-two',
-    tags: [clientTags.webapps],
-  },
-  {
-    title: 'Unicorns and Hippos',
-    subtitle: 'A platform for companies to find and hire freelancers.',
-    logo: 'https://cryptologos.cc/logos/uniswap-uni-logo.png',
-    image:
-      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
-    description:
-      'Unicorns and Hippos is a Swedish startup that provides a platform for companies to find and hire freelancers.',
-    link: 'https://appstract.se',
-    slug: 'unicorns-and-hippos',
-    tags: [clientTags.webapps, clientTags.seo],
-  },
+  // {
+  //   title: 'Client2',
+  //   subtitle: 'A swedish startup that fights food waste.',
+  //   logo: 'https://justifiedcoding.org/wp-content/uploads/2019/01/Client-Logo-Placeholder.png',
+  //   image:
+  //     'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
+  //   description:
+  //     'Client2 is a Swedish startup that fights food waste by selling surplus food from restaurants, cafes and grocery stores at a lower price.',
+  //   link: 'https://appstract.se',
+  //   slug: 'client-two',
+  //   tags: [clientTags.webapps],
+  // },
+  // {
+  //   title: 'Unicorns and Hippos',
+  //   subtitle: 'A platform for companies to find and hire freelancers.',
+  //   logo: 'https://cryptologos.cc/logos/uniswap-uni-logo.png',
+  //   image:
+  //     'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
+  //   description:
+  //     'Unicorns and Hippos is a Swedish startup that provides a platform for companies to find and hire freelancers.',
+  //   link: 'https://appstract.se',
+  //   slug: 'unicorns-and-hippos',
+  //   tags: [clientTags.webapps, clientTags.seo],
+  // },
 ];
 
 export const common = {
