958e7450669df11c1ef5080e83921a65763b013b
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-06-10 20:29:51 +0200

    cleaning up a bit

--- src/components/sections/ProjectsSection.svelte
+++ src/components/sections/ProjectsSection.svelte
@@ -7,7 +7,7 @@
 
 <section>
 	<div class="py-12 mx-auto space-y-8">
-		<div class="max-w-screen-xl mx-auto space-y-4">
+		<div class="max-w-screen-xl px-4 mx-auto space-y-4 xl:px-0">
 			<h2>Kundprojekt</h2>
 			<p>Få en inblick i hur vi jobbar med våra kunder. Från idé till färdig produkt.</p>
 		</div>
--- src/components/swiper/SwiperProjects.svelte
+++ src/components/swiper/SwiperProjects.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-	interface Project {
-		title: string;
-		description: string;
-		subtitle: string;
-		image: string;
-		href: string;
-	}
-	export let projects: Project[] = [];
 	import { onMount } from 'svelte';
 	import Swiper from 'swiper/bundle';
 	import 'swiper/css';
@@ -14,7 +6,18 @@
 	import { Navigation, Pagination } from 'swiper/modules';
 	import 'swiper/scss/pagination';
 	import Button from '../button/Button.svelte';
+
+	interface Project {
+		title: string;
+		description: string;
+		subtitle: string;
+		image: string;
+		href: string;
+	}
+
+	export let projects: Project[] = [];
 	let swiper: Swiper;
+
 	onMount(() => {
 		swiper = new Swiper('.mySwiper', {
 			modules: [Navigation, Pagination],
@@ -37,22 +40,26 @@
 <div class="swiper mySwiper">
 	<div class="max-w-screen-xl mx-auto swiper-wrapper">
 		{#each projects as project, i}
-			<div class="rounded-md swiper-slide">
-				<div class="flex max-w-screen-xl gap-16 mt-2">
-					<div class=" basis-5/12 aspect-[1] outline outline-4 rounded-lg outline-[#697ba8]">
-						<img class="w-full h-full max-w-full rounded-md" src={project.image} alt="SvelteKit" />
+			<div class="swiper-slide">
+				<div class="swiper-slide-content">
+					<div class="basis-5/12 aspect-[1] outline outline-4 rounded-md outline-[#697ba8]">
+						<img src={project.image} alt="SvelteKit" />
 					</div>
-					<div class="flex flex-col justify-center space-y-4 basis-7/12">
-						<h4>
-							<a href={project.href}>
-								{project.subtitle}
-							</a>
-						</h4>
-						<h3>{project.title}</h3>
-						<p>
-							{project.description}
-						</p>
-						<div class="flex mt-8">
+					<div class="flex flex-col justify-center basis-7/12">
+						<div class="space-y-4">
+							<h4>
+								<a href={project.href}>
+									{project.subtitle}
+								</a>
+							</h4>
+							<h3>
+								<a href={project.href}>{project.title}</a>
+							</h3>
+							<p>
+								{project.description}
+							</p>
+						</div>
+						<div class="block mt-12 md:flex">
 							<Button variation="outline" title="Läs mer" href="/" size="md" />
 						</div>
 					</div>
@@ -64,6 +71,18 @@
 </div>
 
 <style lang="scss">
+	.swiper-slide {
+		@apply px-4 xl:px-0;
+	}
+	.swiper-slide-content {
+		@apply flex flex-col max-w-screen-xl gap-6 mt-2 sm:gap-12 lg:gap-16 md:flex-row;
+	}
+	a {
+		@apply hover:underline hover:underline-offset-4;
+	}
+	img {
+		@apply w-full h-full max-w-full rounded-sm;
+	}
 	h4 {
 		@apply uppercase tracking-wider text-xxs;
 	}
@@ -79,7 +98,7 @@
 		@apply text-2xl font-bold;
 	}
 	p {
-		@apply text-base leading-normal;
+		@apply text-base text-balance leading-normal;
 	}
 	.swiper-pagination {
 		@apply my-8;
